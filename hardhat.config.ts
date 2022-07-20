efe2a2d01c6d15c72bd9ac86eba7f71b40573338
0xPeregrinus <0xPeregrinus@proton.me>
2022-07-20 13:03:43 +0700

    Contracts are ready to be deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.6.12',
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts:
+        process.env.PRIVATE_KEY === undefined
+          ? undefined
+          : [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
